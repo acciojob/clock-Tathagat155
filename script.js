a8d05bde023facd4a2414f10f7780cdf232c3627
Tathagat155 <tathagatkumar70618@gmail.com>
2025-11-25 12:20:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function showDateTime() {
+    const now = new Date();
+    const formatted = now.toLocaleString('en-US'); 
+    // This gives → "2/27/2023, 1:04:47 PM"
+
+    document.getElementById("timer").textContent = formatted;
+}
+
+setInterval(showDateTime, 1000);
+showDateTime();  // to show immediately on load
